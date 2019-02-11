d0d198cbeffdf9bd8b52ddbd4a22ee19bca33d4a
pakasyk <mindaugas.krizonis@gmail.com>
2019-02-11 22:56:34 +0200

    added multiple type

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,8 @@ class App extends Component {
       ) {
         this.setState({
           number: 0,
-          score: 0
+          score: 0,
+          questions: [{ question: "Loading..." }]
         });
         this.getQuestions();
       } else {
@@ -38,8 +39,8 @@ class App extends Component {
   getQuestions = () => {
     Trivia.get("api.php", {
       params: {
-        amount: 10,
-        type: "boolean"
+        amount: 10
+        // type: "boolean"
       }
     })
       .then(response => {
@@ -49,12 +50,83 @@ class App extends Component {
   };
 
   render() {
-    const renderedQuestions = this.state.questions.map((question, index) => (
-      <h3 key={index}>
-        {index + 1}.{" "}
-        {question.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'")}
-      </h3>
-    ));
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    const renderedQuestions = this.state.questions.map((question, index) => {
+      if (question.type === 'boolean'){
+        return (
+          <div>
+          <h3 key={index}>
+          {index + 1}.{" "}
+          {question.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'")}
+        </h3>
+        <div className="buttons d-flex" style={{ marginTop: "80px" }}>
+        <button
+          type="button"
+          className="btn btn-success btn-lg btn-block"
+          value="True"
+          onClick={this.onButtonClick}
+        >
+          True
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger btn-lg btn-block"
+          value="False"
+          onClick={this.onButtonClick}
+          style={{ margin: 0 }}
+        >
+          False
+        </button>
+      </div>
+      </div>
+        );
+      }else if (question.type === 'multiple'){
+       
+        let answers = question.incorrect_answers.map((answer, index) => (
+          <button
+          key={index}
+          type="button"
+          className="btn btn-primary btn-lg btn-block"
+          onClick={this.onButtonClick}
+          value={answer}
+          style={{ margin: 0 }}
+        >
+          {answer}
+        </button>
+        ))
+
+        let correct = <button
+        key="3"
+        type="button"
+        className="btn btn-primary btn-lg btn-block"
+        onClick={this.onButtonClick}
+        value={question.correct_answer}
+        style={{ margin: 0 }}
+      >
+        {question.correct_answer}
+      </button>;
+
+      answers.splice(getRandomInt(answers.length+1), 0, correct);
+        return (
+          <div>
+          <h3 key={index}>
+          {index + 1}.{" "}
+          {question.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'")}
+        </h3>
+        <div className="buttons d-flex" style={{ marginTop: "80px" }}>
+        {answers}
+       
+      </div>
+      </div>
+        )}
+        
+     
+      // if(question.type === 'multiple'){
+      //   <button className="btn btn-primary">labas</button>
+      // }
+    });
 
     return (
       <div className="container">
@@ -64,25 +136,7 @@ class App extends Component {
         >
           <h1 className="display-4 text-right">score: {this.state.score}</h1>
           {renderedQuestions[this.state.number]}
-          <div className="buttons d-flex" style={{ marginTop: "80px" }}>
-            <button
-              type="button"
-              className="btn btn-success btn-lg btn-block"
-              value="True"
-              onClick={this.onButtonClick}
-            >
-              True
-            </button>
-            <button
-              type="button"
-              className="btn btn-danger btn-lg btn-block"
-              value="False"
-              onClick={this.onButtonClick}
-              style={{ margin: 0 }}
-            >
-              False
-            </button>
-          </div>
+          
         </div>
       </div>
     );
